740470e8f42b85d0daa09691e7c5aea0fb3af749
svoyak <svoyak78@mail.ru>
2017-12-13 08:51:43 +0200

    Packagesource

---
+++ PackagesSource/dbforge-ora-cb-pro/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/oracle/compare-bundle/comparebundleoracletrial.exe' 
+$url64      = '' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'dbforge-ora-cb-pro' 
+  checksum      = '68CF3877E32978C0711C48A315C0D7A9EF405A40ACE7A4E99D755A650F01B68E'
+  checksumType  = 'sha256' 
+  silentArgs    = "/VERYSILENT"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
