b57f8e3adfde45442e93385285cb69f7a6dfc75a
svoyak <svoyak78@mail.ru>
2017-12-13 10:18:04 +0200

    Packagesource

---
+++ PackagesSource/dbforge-sql-sc-pro/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/sql/schemacompare/schemacomparesql44pro.exe' 
+$url64      = '' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'dbforge-sql-sc-pro' 
+  checksum      = '2F90C8AFA759180EB1E7D9FEC1C37DBEC80F7E1DEDEEB6837C69BAB7045AA276'
+  checksumType  = 'sha256' 
+  silentArgs    = "/VERYSILENT"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
---
+++ PackagesSource/dbforge-sql-sc-std/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/sql/schemacompare/schemacomparesql44std.exe' 
+$url64      = '' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'dbforge-sql-sc-std' 
+  checksum      = '171B4C3A816AED8A31709E43F1689DC0CAEA4CE3C42CCE0B90A0E2B3D08F4429'
+  checksumType  = 'sha256' 
+  silentArgs    = "/VERYSILENT"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
