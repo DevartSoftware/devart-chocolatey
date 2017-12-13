1aa21ed386799c876fc070e9eb2a8162ba9fd854
svoyak <svoyak78@mail.ru>
2017-12-13 10:54:35 +0200

    packagesource

---
+++ PackagesSource/dbforge-sql-decrypt/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/sql/sqldecryptor/sqldecryptor.exe' 
+$url64      = '' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'dbforge-sql-decrypt' 
+  checksum      = 'F7E0D1819E4D9BE70BAB3557562EA69F8D5FB580904A7C18EB3BD100A514D1E4'
+  checksumType  = 'sha256' 
+  silentArgs    = "/VERYSILENT"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
