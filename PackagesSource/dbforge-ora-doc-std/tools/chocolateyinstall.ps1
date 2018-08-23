fd621bc63e319d16b4c807b50857e5306c7eedea
svoyak <svoyak78@mail.ru>
2018-08-23 17:13:41 +0300

    add dbforge-ora-doc-std

---
+++ PackagesSource/dbforge-ora-doc-std/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/oracle/documenter/documenteroracle10.exe' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url 
+  softwareName  = 'dbForge Documenter for Oracle, v1.0.57 Standard Edition' 
+  checksum      = '8508ECF9CB20250B04DABE80F6E12DE79E225DE2375A6E4AEDF7681E9832B79B'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
