cc5ec5bb0b0f4c4bd791522b768a89de021da7f3
JordanS22 <submit@devart.com>
2018-12-21 16:24:05 +0200

    pg studio package upload

---
+++ PackagesSource/dbforge-postgre-studio/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/postgresql/studio/dbforgepostgresql20.exe' 
+$url64      = '' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'dbForge Studio for PostgreSQL, v.2.0.111 Standard (Trial)' 
+  checksum      = 'DDA320935E460F442DFC462E11FE04CE21ADD2DF4A957F3A832001307890529B'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
