027d96c40608f92e4bfed85a12af94e7a564b1ab
JordanS22 <submit@devart.com>
2019-05-28 16:34:20 +0300

    new version

---
+++ PackagesSource/dbforge-ora-studio-ent/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/oracle/studio/dbforgeoracle40.exe' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url 
+  softwareName  = 'Devart dbForge Studio for Oracle, v4.0.237 Enterprise Edition' 
+  checksum      = '21A353928B1E0407A78B4B8FAB0FE6B2B284BDB9E721617ED69A015AE80B2538'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
