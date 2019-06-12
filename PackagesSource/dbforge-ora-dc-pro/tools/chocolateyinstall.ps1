795d2ce868385933998ea9634e00a4840137d238
JordanS22 <submit@devart.com>
2019-06-12 16:05:19 +0300

    Add files via upload

---
+++ PackagesSource/dbforge-ora-dc-pro/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/oracle/compare-bundle/comparebundleoracle.exe' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url 
+  softwareName  = 'Devart dbForge Data Compare for Oracle, v5.1' 
+  checksum      = '5735DF24DE02BCD7335526B95D48E7C3CF83595CDE8FC14395DD3A1A1E4F1E6C'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
