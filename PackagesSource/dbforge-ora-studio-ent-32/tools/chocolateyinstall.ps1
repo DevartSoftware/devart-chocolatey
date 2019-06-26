c9b12d407a3881572bc33b236753a406a2d58b2f
JordanS22 <submit@devart.com>
2019-06-26 16:39:53 +0300

    Updated version

--- PackagesSource/dbforge-ora-studio-ent-32/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-ora-studio-ent-32/tools/chocolateyinstall.ps1
@@ -1,13 +1,11 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://www.devart.com/dbforge/oracle/studio/dbforgeoracle41x86.exe' 
-$url64      = '' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
-  url           = $url
-  url64bit      = $url64
+  url           = $url 
   softwareName  = 'Devart dbForge Studio for Oracle, v4.1.48 Enterprise Edition 32-bit' 
   checksum      = 'C8DFEEB9D03423107AB36D76E5D5B5BE667D74F962DD7CD365094E9021281171'
   checksumType  = 'sha256' 
