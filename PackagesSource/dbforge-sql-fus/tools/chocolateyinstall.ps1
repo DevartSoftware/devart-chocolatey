67e0a81fe8227f2a710260249d2f622f393f1ae2
JordanS22 <submit@devart.com>
2019-11-08 16:34:05 +0200

    New version

---
+++ PackagesSource/dbforge-sql-fus/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/sql/fusion/dbforgesqlvs10.exe' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url 
+  softwareName  = 'Devart dbForge Fusion for SQL Server, v1.11.10 Trial Edition' 
+  checksum      = 'AE9A095A80C071D41823955C23459174C4BE7888FE5C97AC32B86F943AE00DEE'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
