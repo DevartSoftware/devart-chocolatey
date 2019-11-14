3a0dc7e7b137ce8acc0b8d6a7363be8e171fc75a
JordanS22 <submit@devart.com>
2019-11-14 12:08:41 +0200

    New product download

---
+++ PackagesSource/dbforge-sql-mon/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/sql/monitor/monitorsql.exe' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url 
+  softwareName  = 'dbForge Monitor for SQL Server, v1.2 ' 
+  checksum      = 'F5E1C4717045B418441BD47E37D2614E7AEE03F0430C6EFC3BDBC83C65EFB450'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
