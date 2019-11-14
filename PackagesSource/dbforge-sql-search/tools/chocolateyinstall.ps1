bd1c0e8a8e1706ca0066cfdfec36850c2342f1cc
JordanS22 <submit@devart.com>
2019-11-14 12:53:39 +0200

    New product download

---
+++ PackagesSource/dbforge-sql-search/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/sql/search/searchsql24std.exe' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url 
+  softwareName  = 'dbForge Search for SQL Server, v2.4 ' 
+  checksum      = '9D75220A81B4EBAA921F6DEF24F850E2785E54282F0225648C347064CB24E3AE'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
