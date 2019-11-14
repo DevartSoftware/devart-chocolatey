8121f50df9c93947aef643c66015a71ce0666bcf
JordanS22 <submit@devart.com>
2019-11-14 16:56:08 +0200

    New product download (SourceControl)

---
+++ PackagesSource/dbforge-sql-sc/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/sql/developer-bundle/developerbundlesql.exe' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url 
+  softwareName  = 'dbForge Source Control for SQL Server, v2.0 Standard Edition' 
+  checksum      = '781431FD546CA1AE6F547FFD1DE58D356AAB7F442F54C1D3A1FE528C307F4FEA'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
