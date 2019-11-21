3f33ca118d41c264a5551b4f65b34429007d0846
JordanS22 <submit@devart.com>
2019-11-21 10:05:18 +0200

    New version

--- PackagesSource/dbforge-mysql-fus/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-fus/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.devart.com/dbforge/mysql/fusion/dbforgemysqlvs10.exe' 
+$url        = 'https://www.devart.com/dbforge/mysql/fusion/dbforgemysql65vs17.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
   softwareName  = 'Devart dbForge Fusion for MySQL, v6.5.15 Trial Edition' 
-  checksum      = 'AD19055DE26F027A799B7A280BBEC6BF53CA700E9B73516B5D5DE3A58F96E9CF'
+  checksum      = 'BABD8BBBB8FD6BFC9F2068B6B6C022BE2FA1BD23E6508C763053AC46C9ADA256'
   checksumType  = 'sha256' 
   silentArgs    = '/VERYSILENT'
   validExitCodes= @(0, 3010, 1641)
