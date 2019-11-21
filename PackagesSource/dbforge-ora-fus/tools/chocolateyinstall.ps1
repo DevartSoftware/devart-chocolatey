1a8e2f58331a9368907fd2209a8d39265b686c4a
JordanS22 <submit@devart.com>
2019-11-21 10:31:32 +0200

    New version

--- PackagesSource/dbforge-ora-fus/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-ora-fus/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.devart.com/dbforge/oracle/fusion/dbforgeoraclevs10.exe' 
+$url        = 'https://www.devart.com/dbforge/oracle/fusion/dbforgeoracle39vs17.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
   softwareName  = 'Devart dbForge Fusion for Oracle, v3.9.12 Trial Edition' 
-  checksum      = 'E4CA43FB85BE2162070169E619BE15DE135A4ABFDBD411E3CCA454411A263A24'
+  checksum      = '5E15E390BDA84F3B72E8D626DCBF43452066EAA2D1E593E27019A8797417F77A'
   checksumType  = 'sha256' 
   silentArgs    = '/VERYSILENT'
   validExitCodes= @(0, 3010, 1641)
