1459c454f08337c50ec01412c1f9f3e10b134ad2
Stanislav Odrinskiy <StanislavO@devart.com>
2021-09-06 19:54:05 +0300

    Add new packet: dbForge DevOps Automation PowerShell for SQL Server (dbforge-sql-devops)

---
+++ PackagesSource/dbforge-sql-devops/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/sql/compare-bundle/comparebundlesqlpro.exe' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url 
+  softwareName  = 'dbForge Schema Compare for SQL Server, v5.0 Professional Edition' 
+  checksum      = '4BDB4891F35040261C894B9125B36425B93F550E44EA8E64D4C401D71459A870'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
