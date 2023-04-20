f300610ad21381aac62a914c9e3d6788afbb5852
Alexey-Turchin <91960990+Alexey-Turchin@users.noreply.github.com>
2023-04-20 17:33:25 +0300

    rename dbforge-mysql-doc-std ->dbforge-mysql-doc

---
+++ PackagesSource/dbforge-mysql-doc/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.devart.com/dbforge/mysql/documenter/documentermysql11std.exe' 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url 
+  softwareName  = 'dbForge Documenter for MySQL Server, v1.1.10 Standard Edition' 
+  checksum      = 'B7F2C2D28C0FE03AF82D46F956E249C6138F525DB85C84454A7AF656EAFA7C6D'
+  checksumType  = 'sha256' 
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs 
