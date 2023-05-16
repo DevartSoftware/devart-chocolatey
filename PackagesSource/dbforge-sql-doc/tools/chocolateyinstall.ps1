c113d7d3be8d353f1bf1ee414509e9c3a7651c38
Alexey-Turchin <alexeyt@devart.com>
2023-05-16 15:43:24 +0300

    Update PackagesSource dbForge Documenter for SQL Server, v1.7.22

--- PackagesSource/dbforge-sql-doc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-doc/tools/chocolateyinstall.ps1
@@ -1,17 +1,15 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.devart.com/dbforge/sql/documenter/documentersql.exe' 
-$url64      = '' 
+$url        = 'https://choco.devart.com/s/2802C689EA2CE353F45A0C8337495A4373F2457B6F2D1737F0CC9AB36D760089/documentersql17.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
-  url           = $url
-  url64bit      = $url64
-  softwareName  = 'dbForge Documenter for SQL Server, v1.2.72 Standard Edition' 
-  checksum      = 'B63F2C2BC2EB0F99186265DCFBAC9532CE6D3E8815AEEBEEA7312D0191AF2ED3'
+  url           = $url 
+  softwareName  = 'dbForge Documenter for SQL Server, v1.7.22' 
+  checksum      = '2802C689EA2CE353F45A0C8337495A4373F2457B6F2D1737F0CC9AB36D760089'
   checksumType  = 'sha256' 
-  silentArgs    = '/VERYSILENT'
+  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1641)
 }
 Install-ChocolateyPackage @packageArgs 
