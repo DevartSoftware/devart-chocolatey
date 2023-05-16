fa803ff69b1fd913ee594723f656a950b710eedf
Alexey-Turchin <alexeyt@devart.com>
2023-05-16 15:23:06 +0300

    Update PackagesSource dbForge Data Pump for SQL Server, v1.8.21

--- PackagesSource/dbforge-sql-dp/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-dp/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.devart.com/dbforge/sql/developer-bundle/developerbundlesql.exe' 
+$url        = ' http://choco.devart.com/s/4099F45B8A02F8A1FBF0DB678C07A0B793FCA9B78B324C146BB4E2B4564240DD/datapumpsql18.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Data Pump for SQL Server, v1.5 Standard Edition' 
-  checksum      = '781431FD546CA1AE6F547FFD1DE58D356AAB7F442F54C1D3A1FE528C307F4FEA'
+  softwareName  = 'dbForge Data Pump for SQL Server, v1.8.21' 
+  checksum      = '4099F45B8A02F8A1FBF0DB678C07A0B793FCA9B78B324C146BB4E2B4564240DD'
   checksumType  = 'sha256' 
-  silentArgs    = '/VERYSILENT'
+  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1641)
 }
 Install-ChocolateyPackage @packageArgs 
