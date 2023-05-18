9d3e3dbfc81aa73363d90c831f9182585d12ac64
Alexey-Turchin <alexeyt@devart.com>
2023-05-18 17:58:59 +0300

    Update PackagesSource dbForge Schema Compare for MySQL, v5.2.29

--- PackagesSource/dbforge-mysql-sc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-sc/tools/chocolateyinstall.ps1
@@ -1,17 +1,15 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.devart.com/dbforge/mysql/schemacompare/schemacomparemysql43std.exe' 
-$url64      = '' 
+$url        = 'https://choco.devart.com/s/2B9F59482765F923A21A4418C785C88A3F885F2EA158A568514585145A65E3A3/schemacomparemysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
-  url           = $url
-  url64bit      = $url64
-  softwareName  = 'Devart dbForge Schema Compare for MySQL, v4.3 Trial Edition' 
-  checksum      = '311967912381063735CB36E5CD668FE2BA6D763DD1F84960FA9B5D090EAF2528'
+  url           = $url 
+  softwareName  = 'dbForge Schema Compare for MySQL, v5.2.29' 
+  checksum      = '2B9F59482765F923A21A4418C785C88A3F885F2EA158A568514585145A65E3A3'
   checksumType  = 'sha256' 
-  silentArgs    = '/VERYSILENT'
+  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1641)
 }
 Install-ChocolateyPackage @packageArgs 
