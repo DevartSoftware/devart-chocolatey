e0aa78ad39d572ba1b4c1bf60671ad4e42956492
Alexey-Turchin <alexeyt@devart.com>
2023-05-18 18:00:51 +0300

    Update PackagesSource dbForge Studio 2022 for MySQL, v9.2.34

--- PackagesSource/dbforge-mysql-studio/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-studio/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://choco.devart.com/s/156F7D1309AA7691EAD9DBF0D02F01D84D92285362B6DBFCD26D5D0B189AEE26/dbforgemysql90.exe' 
+$url        = 'https://choco.devart.com/s/54B71560BB8E948B46E0CA62A58D880E1EE1F73B253AE90283F47B053C064A45/dbforgemysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Studio 2020 for MySQL, v9.0.304' 
-  checksum      = '156F7D1309AA7691EAD9DBF0D02F01D84D92285362B6DBFCD26D5D0B189AEE26'
+  softwareName  = 'dbForge Studio 2022 for MySQL, v9.2.34' 
+  checksum      = '54B71560BB8E948B46E0CA62A58D880E1EE1F73B253AE90283F47B053C064A45'
   checksumType  = 'sha256' 
-  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS'
+  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1641)
 }
 Install-ChocolateyPackage @packageArgs 
--- PackagesSource/dbforge-mysql-studio/tools/chocolateyuninstall.ps1
+++ PackagesSource/dbforge-mysql-studio/tools/chocolateyuninstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'dbForge Studio 2020 for MySQL, v9.0.304'
+  softwareName  = 'dbForge Studio 2022 for MySQL, v9.2.34'
   fileType      = 'exe'
 
-  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS'
+  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1605, 1614, 1641)  
 }
 
