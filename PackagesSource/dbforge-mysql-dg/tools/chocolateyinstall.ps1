570d68d31269aad388c87239f5463685c63eae35
Alexey-Turchin <alexeyt@devart.com>
2023-05-18 17:32:37 +0300

    Update PackagesSource dbForge Data Generator for MySQL, v2.6.27

--- PackagesSource/dbforge-mysql-dg/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-dg/tools/chocolateyinstall.ps1
@@ -1,17 +1,15 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.devart.com/dbforge/mysql/data-generator/datageneratormysql21std.exe' 
-$url64      = '' 
+$url        = 'https://choco.devart.com/s/977CE47BA46593B6348F8409E1651E3B8F05199BEDD645046BDD4395971750D9/datageneratormysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
-  url           = $url
-  url64bit      = $url64
-  softwareName  = 'Devart dbForge Data Generator for MySQL, v2.1 Trial Edition' 
-  checksum      = '972B449BF3AA9777B9BAD65CD24F519F6119DD3551CC8343225A7D99BB9C2533'
+  url           = $url 
+  softwareName  = 'dbForge Data Generator for MySQL, v2.6.27' 
+  checksum      = '977CE47BA46593B6348F8409E1651E3B8F05199BEDD645046BDD4395971750D9'
   checksumType  = 'sha256' 
-  silentArgs    = '/VERYSILENT'
+  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1641)
 }
 Install-ChocolateyPackage @packageArgs 
