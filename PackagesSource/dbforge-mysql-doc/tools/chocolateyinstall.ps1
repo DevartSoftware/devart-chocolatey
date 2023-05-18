5f26206a12b3765a3d4d58012be6e9d3963e5b0c
Alexey-Turchin <alexeyt@devart.com>
2023-05-18 17:47:18 +0300

    Update PackagesSource dbForge Documenter for MySQL, v2.2.27

--- PackagesSource/dbforge-mysql-doc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-doc/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/2B83AA821E4557560A674C4DE62976A927B938056E300CDCD174576CE50C9FE6/documentermysql.exe' 
+$url        = 'https://choco.devart.com/s/609EC460C39BA27F2E27D25D3A6F1096A0AB67D7C6C29C2E08463D0D7883CB38/documentermysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Documenter for MySQL, v2.2.4' 
-  checksum      = '2B83AA821E4557560A674C4DE62976A927B938056E300CDCD174576CE50C9FE6'
+  softwareName  = 'dbForge Documenter for MySQL, v2.2.27' 
+  checksum      = '609EC460C39BA27F2E27D25D3A6F1096A0AB67D7C6C29C2E08463D0D7883CB38'
   checksumType  = 'sha256' 
   silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1641)
--- PackagesSource/dbforge-mysql-doc/tools/chocolateyuninstall.ps1
+++ PackagesSource/dbforge-mysql-doc/tools/chocolateyuninstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'dbForge Documenter for MySQL, v2.2.4'
+  softwareName  = 'dbForge Documenter for MySQL, v2.2.27'
   fileType      = 'exe'
 
-  silentArgs    = '/VERYSILENT'
+  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1605, 1614, 1641)  
 }
 
