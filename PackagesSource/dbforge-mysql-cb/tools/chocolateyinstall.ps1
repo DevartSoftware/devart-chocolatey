6815a76a30dc159db59e8fd44526ccd8f98ca98e
Alexey-Turchin <alexeyt@devart.com>
2023-05-18 18:02:51 +0300

    Update PackagesSource Compare Bundle for MySQL Standard

--- PackagesSource/dbforge-mysql-cb/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-cb/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://choco.devart.com/s/017D4A5FEF0AFBEE6F9A5EE4FF1116F051C9F9FD5D1DE3FC96908BB55700660F/comparebundlemysqlstd.exe' 
+$url        = 'https://choco.devart.com/s/6DC2CA05D0E9F321187139866149B6055A598B478CC14973C67AB6BB498EC878/comparebundlemysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
   softwareName  = 'Compare Bundle for MySQL Standard' 
-  checksum      = '017D4A5FEF0AFBEE6F9A5EE4FF1116F051C9F9FD5D1DE3FC96908BB55700660F'
+  checksum      = '6DC2CA05D0E9F321187139866149B6055A598B478CC14973C67AB6BB498EC878'
   checksumType  = 'sha256' 
   silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1641)
