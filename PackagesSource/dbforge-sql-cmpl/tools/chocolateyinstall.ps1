35724930f14f0c54f9017a7ee49a69f5b31eccf7
Alexey-Turchin <alexeyt@devart.com>
2023-07-24 16:16:19 +0300

    add log in package

--- PackagesSource/dbforge-sql-cmpl/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-cmpl/tools/chocolateyinstall.ps1
@@ -1,25 +1,15 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/17D1C3AA2C3751829DA8520CBD98933AF4EF40C6B6EBABD39BDE7FF2E0578A46/sqlcompletesql614.exe' 
+$url        = 'https://choco.devart.com/s/3B2623296DF407077BCFEC08C5B83E9E21C8D3804EE2260DF32A0F96BCD6A6F9/sqlcompletesql615.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge SQL Complete, v6.14.26' 
-  checksum      = '17D1C3AA2C3751829DA8520CBD98933AF4EF40C6B6EBABD39BDE7FF2E0578A46'
+  softwareName  = 'dbForge SQL Complete, v6.15.5' 
+  checksum      = '3B2623296DF407077BCFEC08C5B83E9E21C8D3804EE2260DF32A0F96BCD6A6F9'
   checksumType  = 'sha256' 
-    silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
+  silentArgs    = '/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0'
   validExitCodes= @(0, 3010, 1641)
 }
-
-  Write-Host "The log file will be saved to: $env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log" -ForegroundColor Yellow
-  Write-Host ""
-  Write-Host "If you are experiencing issues with installing our product, we recommend contacting our support team for assistance." -ForegroundColor Green
-  Write-Host "You can reach out to our support team by visiting the following link:" -ForegroundColor Green
-  Write-Host "https://support.devart.com/portal/en/community" -ForegroundColor Magenta
-  Write-Host ""
-  Write-Host "Additionally, you can refer to the resources and documentation provided by Chocolatey for finding possible solutions and support. You can find more information at:" -ForegroundColor Green
-  Write-Host "https://docs.chocolatey.org/en-us/troubleshooting#mainContent" -ForegroundColor Magenta
-  Write-Host ""
 Install-ChocolateyPackage @packageArgs 
