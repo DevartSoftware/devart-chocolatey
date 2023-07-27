1a378613a46d6da7b1dcdf970851502d9fc4d9bb
Alexey-Turchin <91960990+Alexey-Turchin@users.noreply.github.com>
2023-07-27 14:28:59 +0300

    Update version

--- PackagesSource/dbforge-ora-dc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-ora-dc/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://choco.devart.com/s/53D9805C86F893CA3D7D1AA6BBC24762A6F74C14607C94F34F6E1320B08D35B3/datacompareoracle52.exe' 
+$url        = 'https://choco.devart.com/s/DCD3FBE53F8D4102929D14370BC0155E5D37E6E94CCD8C5A41A33DEB90853E7E/datacompareoracle55.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Data Compare for Oracle, v5.2.8' 
-  checksum      = '53D9805C86F893CA3D7D1AA6BBC24762A6F74C14607C94F34F6E1320B08D35B3'
+  softwareName  = 'dbForge Data Compare for Oracle, v5.5.100' 
+  checksum      = 'DCD3FBE53F8D4102929D14370BC0155E5D37E6E94CCD8C5A41A33DEB90853E7E'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
