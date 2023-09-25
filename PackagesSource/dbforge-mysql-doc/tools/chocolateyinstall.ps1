0afc662cf0a7f58e5933f16833c7415b5f40f383
Alexey-Turchin <91960990+Alexey-Turchin@users.noreply.github.com>
2023-09-25 11:29:31 +0300

    Update version dbforge-mysql-doc+2.2.107

--- PackagesSource/dbforge-mysql-doc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-doc/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/A8A2F85BAFE612FC5AAD549D98115038A23EA8654ED901B59944ED3F46D9F510/documentermysql.exe' 
+$url        = 'https://choco.devart.com/s/FC480546C3F4F963A9FCBD31EE7BA1D80E8AD7BD410C1AB9DA95A57EB9B4CF66/documentermysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Documenter for MySQL, v2.2.84' 
-  checksum      = 'A8A2F85BAFE612FC5AAD549D98115038A23EA8654ED901B59944ED3F46D9F510'
+  softwareName  = 'dbForge Documenter for MySQL, v2.2.107' 
+  checksum      = 'FC480546C3F4F963A9FCBD31EE7BA1D80E8AD7BD410C1AB9DA95A57EB9B4CF66'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
