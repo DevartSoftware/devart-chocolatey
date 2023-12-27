c2951470521276b3dd54965067c7dfe82f6e24b4
unknown <Administrator@choco-build.datasoft.local>
2023-12-27 11:29:58 +0200

    Update package dbforge-mysql-studio to version 10.0.46

--- PackagesSource/dbforge-mysql-studio/tools/chocolateyuninstall.ps1
+++ PackagesSource/dbforge-mysql-studio/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'dbForge Studio for MySQL, v10.0.60'
+  softwareName  = 'dbForge Studio for MySQL, v10.0.46'
   fileType      = 'exe'
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Uninstall.log`""
   validExitCodes= @(0, 3010, 1605, 1614, 1641)  
