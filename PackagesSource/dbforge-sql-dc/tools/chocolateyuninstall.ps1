384e49b3b9cdf03a9be8fbe17d40c9b8ce7656d4
alexeyt <alexeyt@devart.com>
2024-01-02 09:30:38 +0200

    Update package dbforge-sql-dc to version 5.4.9

--- PackagesSource/dbforge-sql-dc/tools/chocolateyuninstall.ps1
+++ PackagesSource/dbforge-sql-dc/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'dbForge Data Compare for SQL Server, v5.4.4'
+  softwareName  = 'dbForge Data Compare for SQL Server, v5.4.9'
   fileType      = 'exe'
 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Uninstall.log`""
