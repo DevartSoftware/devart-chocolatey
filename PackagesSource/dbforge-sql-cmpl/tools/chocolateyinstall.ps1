e616b17b62cf9e850d051b6c24fc0d2308b718f0
Alexey-Turchin <91960990+Alexey-Turchin@users.noreply.github.com>
2024-01-09 13:13:48 +0200

    system requirements
    Mandatory system requirements for successful installation

--- PackagesSource/dbforge-sql-cmpl/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-cmpl/tools/chocolateyinstall.ps1
@@ -12,7 +12,14 @@ $packageArgs = @{
     silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
 }
-
+  Write-Host ""
+  Write-Host "Mandatory system requirements for successful installation:" -ForegroundColor DarkYellow
+  Write-Host "- The tool requires Microsoft SQL Server Management Studio (MS SSMS) 2012 or higher to be installed."
+  Write-Host "https://learn.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver15" -ForegroundColor Magenta
+  Write-Host "- The tool requires Visual Studio 2012 or higher to be installed."
+  Write-Host "https://visualstudio.microsoft.com/downloads/" -ForegroundColor Magenta
+  Write-Host "- Ensure that the programs with which the product interacts are closed prior to installation to avoid conflicts."
+  Write-Host ""
   Write-Host "The log file will be saved to: $env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log" -ForegroundColor Yellow
   Write-Host ""
   Write-Host "If you are experiencing issues with installing our product, we recommend contacting our support team for assistance." -ForegroundColor Green
