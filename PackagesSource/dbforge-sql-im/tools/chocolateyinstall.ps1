bd9363fd0809678cc1d2103ac335173322c0813d
alexeyt <alexeyt@devart.com>
2024-09-09 11:02:31 +0300

    Update package dbforge-sql-im to version 7.0.158

--- PackagesSource/dbforge-sql-im/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-im/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/B98F21F5CCBBA37FDBBE468F1EFBB658F8DC6D06109FBA3B6DC20EE7ECC363FA/indexmanagersql114.exe' 
+$url        = 'https://choco.devart.com/s/1CAB74854F8BC7D9D8BBA0FA2A6DE4E79DF870A111943F162B6EC3C6B68A4F05/indexmanagersql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Index Manager for SQL Server, v1.14.2' 
-  checksum      = 'B98F21F5CCBBA37FDBBE468F1EFBB658F8DC6D06109FBA3B6DC20EE7ECC363FA'
+  softwareName  = 'dbForge Index Manager for SQL Server, v7.0.158' 
+  checksum      = '1CAB74854F8BC7D9D8BBA0FA2A6DE4E79DF870A111943F162B6EC3C6B68A4F05'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
