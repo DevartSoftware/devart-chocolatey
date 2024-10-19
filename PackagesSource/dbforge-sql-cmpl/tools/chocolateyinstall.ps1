8455a8e4b94bd09b67795fbec5e585edaa32dd03
site <forit@devart.com>
2024-10-19 17:12:49 +0300

    Update package dbforge-sql-cmpl to version 7.0.210

--- PackagesSource/dbforge-sql-cmpl/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-cmpl/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/9760349C3F42D1C4356F20615072BF58C1464A8D57C09F9D7A0B401DCBEFB5CB/sqlcompletesql.exe' 
+$url        = 'https://choco.devart.com/s/6B61311ECE49281681FF26FD3A7692768407130644824C85BE95AA8FB6B62706/sqlcompletesql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge SQL Complete, v7.0.158' 
-  checksum      = '9760349C3F42D1C4356F20615072BF58C1464A8D57C09F9D7A0B401DCBEFB5CB'
+  softwareName  = 'dbForge SQL Complete, v7.0.210' 
+  checksum      = '6B61311ECE49281681FF26FD3A7692768407130644824C85BE95AA8FB6B62706'
   checksumType  = 'sha256' 
     silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
