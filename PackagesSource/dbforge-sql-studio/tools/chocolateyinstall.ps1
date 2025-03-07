965028bbfc589ad0efced97e90d94f189e1cb0cf
site <forit@devart.com>
2025-03-07 15:55:53 +0200

    Update package dbforge-sql-studio to version 7.0.362

--- PackagesSource/dbforge-sql-studio/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-studio/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/AAA3612A371FBB3CE2DAAEBF9701937712DA632988ACF2BE74739EC2D8E9E053/dbforgesql.exe' 
+$url        = 'https://choco.devart.com/s/1B0798C1ACDA284F0F82339F31108952E9E9AF89BCE9A9E57229B21BDF5AD238/dbforgesql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Studio for SQL Server, v7.0.249' 
-  checksum      = 'AAA3612A371FBB3CE2DAAEBF9701937712DA632988ACF2BE74739EC2D8E9E053'
+  softwareName  = 'dbForge Studio for SQL Server, v7.0.362' 
+  checksum      = '1B0798C1ACDA284F0F82339F31108952E9E9AF89BCE9A9E57229B21BDF5AD238'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
