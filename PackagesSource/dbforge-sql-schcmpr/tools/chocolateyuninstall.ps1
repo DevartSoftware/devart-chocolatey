3e775e88b9cfc456899b3472e2ea5b7daca2a09a
site <forit@devart.com>
2025-06-17 11:31:30 +0300

    Update package dbforge-sql-schcmpr to version 2025.1.124

--- PackagesSource/dbforge-sql-schcmpr/tools/chocolateyuninstall.ps1
+++ PackagesSource/dbforge-sql-schcmpr/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'dbForge Schema Compare for SQL Server, v7.1.63'
+  softwareName  = 'dbForge Schema Compare for SQL Server, v2025.1.124'
   fileType      = 'exe'
 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Uninstall.log`""
