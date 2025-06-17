ea9c320bb998539662e13dee51aba9db7ffd43be
site <forit@devart.com>
2025-06-17 11:20:46 +0300

    Update package dbforge-sql-dp to version 2025.1.124

--- PackagesSource/dbforge-sql-dp/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-dp/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/F33A20E91C1810F26B5620DC01EB47DDE4F5F6669BC35A0FB11A31C62859D457/datapumpsql.exe' 
+$url        = 'https://choco.devart.com/s/6D689187696AEBC4EE5765D232F259F9E3781B3A487D66DDCCC151E1A1836AB2/datapumpsql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Data Pump for SQL Server, v7.1.63' 
-  checksum      = 'F33A20E91C1810F26B5620DC01EB47DDE4F5F6669BC35A0FB11A31C62859D457'
+  softwareName  = 'dbForge Data Pump for SQL Server, v2025.1.124' 
+  checksum      = '6D689187696AEBC4EE5765D232F259F9E3781B3A487D66DDCCC151E1A1836AB2'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
