7c248f3d10f5bbd3dee3dcfe889137303b6307ee
site <forit@devart.com>
2025-06-17 11:28:05 +0300

    Update package dbforge-sql-mon to version 2025.1.124

--- PackagesSource/dbforge-sql-mon/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-mon/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/A4ADFDB10504F06037F06CBE2BEA8595DD790465ECC67F94E5C4AEA46F7ABDE5/monitorsql.exe' 
+$url        = 'https://choco.devart.com/s/0A9475AC8544E0CEBE62D87999740E741D90C3762E9AE4BC729199EE13A62F51/monitorsql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Monitor for SQL Server, v7.1.63' 
-  checksum      = 'A4ADFDB10504F06037F06CBE2BEA8595DD790465ECC67F94E5C4AEA46F7ABDE5'
+  softwareName  = 'dbForge Monitor for SQL Server, v2025.1.124' 
+  checksum      = '0A9475AC8544E0CEBE62D87999740E741D90C3762E9AE4BC729199EE13A62F51'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
