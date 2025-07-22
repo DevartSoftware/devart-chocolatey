5dcac53f0ed193a33dbc2bc4ed4c2795fc00cd28
site <pf_d42@devart.com>
2025-07-22 10:20:43 +0300

    Update package dbforge-mysql-studio to version 2025.1.186

--- PackagesSource/dbforge-mysql-studio/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-studio/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/99A4301194C42A607DFD599881220422D57196A2F356CB5A217980A6D379A32D/dbforgemysql.exe' 
+$url        = 'https://choco.devart.com/s/9133CACFC3CB8FADB17E8DB2A9BCDADDC14601B17F85A24C986603D06687D0B9/dbforgemysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Studio for MySQL, v2025.1.134' 
-  checksum      = '99A4301194C42A607DFD599881220422D57196A2F356CB5A217980A6D379A32D'
+  softwareName  = 'dbForge Studio for MySQL, v2025.1.186' 
+  checksum      = '9133CACFC3CB8FADB17E8DB2A9BCDADDC14601B17F85A24C986603D06687D0B9'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
