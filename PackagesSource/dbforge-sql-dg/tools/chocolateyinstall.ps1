397f7fafac75772f7ee70fdea0259a1eb187d678
site <pf_d42@devart.com>
2025-08-22 16:04:28 +0300

    Update package dbforge-sql-dg to version 2025.1.230

--- PackagesSource/dbforge-sql-dg/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-dg/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/89C48A5A6413E5B2BD57E726172F092BD82B5EBD48B35528CD6ECBEC8758D046/datageneratorsql.exe' 
+$url        = 'https://choco.devart.com/s/08DA2831D189F5381868DC539679F6C0277185E61953573583F97FFF2FFCF2D4/datageneratorsql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Data Generator for SQL Server, v2025.1.186' 
-  checksum      = '89C48A5A6413E5B2BD57E726172F092BD82B5EBD48B35528CD6ECBEC8758D046'
+  softwareName  = 'dbForge Data Generator for SQL Server, v2025.1.230' 
+  checksum      = '08DA2831D189F5381868DC539679F6C0277185E61953573583F97FFF2FFCF2D4'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
