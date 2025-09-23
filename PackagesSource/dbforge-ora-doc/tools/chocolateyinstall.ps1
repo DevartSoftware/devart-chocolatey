45e45726c7cde775a47352fe27e42e78450bf164
site <vitaliybog@devart.com>
2025-09-23 18:51:08 +0300

    Update package dbforge-ora-doc to version 2025.2.37

--- PackagesSource/dbforge-ora-doc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-ora-doc/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/969BE91F9576D64E291B5895FD1F0A0172E6FB6225F6D54E9DFC447FB5C098BC/documenteroracle.exe' 
+$url        = 'https://choco.devart.com/s/1569C200E6AE50C821200357458B37F31FAAB299E19574D12A34682F4C17E303/documenteroracle.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Documenter for Oracle, v2025.1.230' 
-  checksum      = '969BE91F9576D64E291B5895FD1F0A0172E6FB6225F6D54E9DFC447FB5C098BC'
+  softwareName  = 'dbForge Documenter for Oracle, v2025.2.37' 
+  checksum      = '1569C200E6AE50C821200357458B37F31FAAB299E19574D12A34682F4C17E303'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
