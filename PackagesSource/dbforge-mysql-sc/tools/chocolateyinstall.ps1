4531c286743b603ef0b087e9b331b8d88dd5a3eb
site <vitaliybog@devart.com>
2025-09-23 18:29:55 +0300

    Update package dbforge-mysql-sc to version 2025.2.37

--- PackagesSource/dbforge-mysql-sc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-sc/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/FCB514558AD336128EA6EEADE75FD008CA6228A22CF232D08303BA2D03BD02BE/schemacomparemysql.exe' 
+$url        = 'https://choco.devart.com/s/88E2FC49766DA08F7A7012B4E7FBAA586AA2C2A373F6C7D734919490CEDE5135/schemacomparemysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Schema Compare for MySQL, v2025.1.230' 
-  checksum      = 'FCB514558AD336128EA6EEADE75FD008CA6228A22CF232D08303BA2D03BD02BE'
+  softwareName  = 'dbForge Schema Compare for MySQL, v2025.2.37' 
+  checksum      = '88E2FC49766DA08F7A7012B4E7FBAA586AA2C2A373F6C7D734919490CEDE5135'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
