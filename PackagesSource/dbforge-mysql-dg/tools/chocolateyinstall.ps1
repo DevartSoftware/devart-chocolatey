a3d4f9189e5465d5d3a4af0fd34e56546c2cf73e
site <vitaliybog@devart.com>
2025-09-23 18:22:34 +0300

    Update package dbforge-mysql-dg to version 2025.2.37

--- PackagesSource/dbforge-mysql-dg/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-dg/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/24DBCA8AECF7BFB7082A687E40DA20E5F3AFD56C8469EFEC6A8C8DCFF1820D26/datageneratormysql.exe' 
+$url        = 'https://choco.devart.com/s/D7284E566A268EBC3F0AFEA8E568ADBE04074F848C503660DCFAB79B40C5E0B4/datageneratormysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Data Generator for MySQL, v2025.1.230' 
-  checksum      = '24DBCA8AECF7BFB7082A687E40DA20E5F3AFD56C8469EFEC6A8C8DCFF1820D26'
+  softwareName  = 'dbForge Data Generator for MySQL, v2025.2.37' 
+  checksum      = 'D7284E566A268EBC3F0AFEA8E568ADBE04074F848C503660DCFAB79B40C5E0B4'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
