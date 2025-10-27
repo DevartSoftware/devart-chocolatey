f8f57f59c1a53beb07a7019afe092515ae60295f
site <vitaliybog@devart.com>
2025-10-27 16:07:11 +0200

    Update package dbforge-ora-cb to version 2025.2.91

--- PackagesSource/dbforge-ora-cb/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-ora-cb/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/E02E41ACDAC538E3258A4541EB254EC52B46B69133FDEBD7177BE021C8142AA9/comparebundleoracle.exe' 
+$url        = 'https://choco.devart.com/s/6A1C748A4DC804022377012FC9F308D79044AD211640A41196CF1C0A8B704704/comparebundleoracle.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
   softwareName  = 'Compare Bundle for Oracle' 
-  checksum      = 'E02E41ACDAC538E3258A4541EB254EC52B46B69133FDEBD7177BE021C8142AA9'
+  checksum      = '6A1C748A4DC804022377012FC9F308D79044AD211640A41196CF1C0A8B704704'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
