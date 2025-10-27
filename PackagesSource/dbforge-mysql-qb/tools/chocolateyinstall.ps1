a726a5dc1b2bc21856b2714ce799958b2bdec679
site <vitaliybog@devart.com>
2025-10-27 16:01:57 +0200

    Update package dbforge-mysql-qb to version 2025.2.91

--- PackagesSource/dbforge-mysql-qb/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-qb/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/346E0BE649BB8DDA68A736AC8B30CBAFD0ACE5D5FAB589901253B50524956B8F/querybuildermysql.exe' 
+$url        = 'https://choco.devart.com/s/5A6F2FB670ED038DA8D07D24526C74D7FDBE51E61FE1F15BFA0CCD7EF9AC3D99/querybuildermysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Query Builder for MySQL, v2025.2.37' 
-  checksum      = '346E0BE649BB8DDA68A736AC8B30CBAFD0ACE5D5FAB589901253B50524956B8F'
+  softwareName  = 'dbForge Query Builder for MySQL, v2025.2.91' 
+  checksum      = '5A6F2FB670ED038DA8D07D24526C74D7FDBE51E61FE1F15BFA0CCD7EF9AC3D99'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
