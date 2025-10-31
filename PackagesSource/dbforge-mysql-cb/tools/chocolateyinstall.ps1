c8f1b6c3d544a6d2db2ee08ffb45d9c70d6cd519
site <vitaliybog@devart.com>
2025-10-31 20:04:03 +0200

    Update package dbforge-mysql-cb to version 2025.2.109

--- PackagesSource/dbforge-mysql-cb/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-cb/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/C926DAD3323133D6A1CB0C9CA22577F51293162188C984EF937BA615AE045C16/comparebundlemysql.exe' 
+$url        = 'https://choco.devart.com/s/0BF27C407A7DD4F583C98CBC406CCEDC7C88484E0A9C04C3CC5753E6863F8660/comparebundlemysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
   softwareName  = 'Compare Bundle for MySQL Standard' 
-  checksum      = 'C926DAD3323133D6A1CB0C9CA22577F51293162188C984EF937BA615AE045C16'
+  checksum      = '0BF27C407A7DD4F583C98CBC406CCEDC7C88484E0A9C04C3CC5753E6863F8660'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
