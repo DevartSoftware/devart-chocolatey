cb58d736ff7693948bec9e09372c2c87ae0ba249
site <vitaliybog@devart.com>
2025-11-21 16:03:41 +0200

    Update package dbforge-sql-qb to version 2025.2.138

--- PackagesSource/dbforge-sql-qb/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-qb/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/7F09593B25B6860AB63F104B3226313B97D98723A47FDB33629D24354077CB62/qbuildersql.exe' 
+$url        = 'https://choco.devart.com/s/F0334EDEE7178CF1AF264B02BF63F64418FB640BAB1F9E92393C373469A814C8/qbuildersql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Query Builder for SQL Server, v2025.2.109' 
-  checksum      = '7F09593B25B6860AB63F104B3226313B97D98723A47FDB33629D24354077CB62'
+  softwareName  = 'dbForge Query Builder for SQL Server, v2025.2.138' 
+  checksum      = 'F0334EDEE7178CF1AF264B02BF63F64418FB640BAB1F9E92393C373469A814C8'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
