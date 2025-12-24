068f5673f7f581fb92e4283c7a6a45cbecdde549
site <vitaliybog@devart.com>
2025-12-24 16:23:18 +0200

    Update package dbforge-sql-sc to version 2025.3.61

--- PackagesSource/dbforge-sql-sc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-sc/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://choco.devart.com/s/D4AC8AACC27387DF63D7F1048A5D886FD4B53533E4079A19CFC21A0772980166/sourcecontrolsql.exe' 
+$url        = 'https://choco.devart.com/s/CD5CD255E423820871D05EF1FD7E51809D69D4F25D3B17BC6D332DB4DBE7C3C6/sourcecontrolsql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Source Control for SQL Server, v2025.2.138' 
-  checksum      = 'D4AC8AACC27387DF63D7F1048A5D886FD4B53533E4079A19CFC21A0772980166'
+  softwareName  = 'dbForge Source Control for SQL Server, v2025.3.61' 
+  checksum      = 'CD5CD255E423820871D05EF1FD7E51809D69D4F25D3B17BC6D332DB4DBE7C3C6'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
