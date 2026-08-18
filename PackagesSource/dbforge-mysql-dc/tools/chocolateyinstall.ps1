3c0667c348ac84d38719be59fc46270608f931cd
site <vitaliybog@devart.com>
2026-08-18 14:48:00 +0300

    Update package dbforge-mysql-dc to version 2026.1.106

--- PackagesSource/dbforge-mysql-dc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-mysql-dc/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://chocolatey.devart.com/files/B2B85E477AA18F9CB7E2D5917CB190226CCE4649B4AD171527B4800E11351946/datacomparemysql.exe' 
+$url        = 'https://chocolatey.devart.com/files/A994148407C55ACA1B6CE9FDDADA51136CB00BB5ECE790BF9EA7861F6CA459C6/datacomparemysql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Data Compare for MySQL, v2026.1.77' 
-  checksum      = 'B2B85E477AA18F9CB7E2D5917CB190226CCE4649B4AD171527B4800E11351946'
+  softwareName  = 'dbForge Data Compare for MySQL, v2026.1.106' 
+  checksum      = 'A994148407C55ACA1B6CE9FDDADA51136CB00BB5ECE790BF9EA7861F6CA459C6'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
