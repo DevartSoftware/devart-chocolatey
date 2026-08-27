72a935876797c55106c38bc3f1ef91427bc766af
site <vitaliybog@devart.com>
2026-08-27 11:00:59 +0300

    Update package dbforge-sql-dc to version 2026.1.112

--- PackagesSource/dbforge-sql-dc/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-dc/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://chocolatey.devart.com/files/AC72B781F0EA782D0483BFF9E34131DA1CC82030A704D54DAD64B8F04B971DD0/datacomparesql.exe' 
+$url        = 'https://chocolatey.devart.com/files/8DEE76B88118A2435B84EE0E10400871853156318951F227654E4441FADA177F/datacomparesql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Data Compare for SQL Server, v2026.1.106' 
-  checksum      = 'AC72B781F0EA782D0483BFF9E34131DA1CC82030A704D54DAD64B8F04B971DD0'
+  softwareName  = 'dbForge Data Compare for SQL Server, v2026.1.112' 
+  checksum      = '8DEE76B88118A2435B84EE0E10400871853156318951F227654E4441FADA177F'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
