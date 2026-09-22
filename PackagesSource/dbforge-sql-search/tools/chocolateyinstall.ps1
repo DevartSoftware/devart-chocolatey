95ec53ec1dc790523a0d8058ffca4ffd96e51d29
site <vitaliybog@devart.com>
2026-09-22 12:18:29 +0300

    Update package dbforge-sql-search to version 2026.2.54

--- PackagesSource/dbforge-sql-search/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-search/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://chocolatey.devart.com/files/F898FC20D25348ACF0023C4FA950367FED22D5DAC2D11D4FB31D56243931D9A5/searchsql.exe' 
+$url        = 'https://chocolatey.devart.com/files/532D4B841AF9382D7B4C7137FC53F42377B72D64BDE3EEF6656CD067F8FA527E/searchsql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Search for SQL Server, v2026.1.112' 
-  checksum      = 'F898FC20D25348ACF0023C4FA950367FED22D5DAC2D11D4FB31D56243931D9A5'
+  softwareName  = 'dbForge Search for SQL Server, v2026.2.54' 
+  checksum      = '532D4B841AF9382D7B4C7137FC53F42377B72D64BDE3EEF6656CD067F8FA527E'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
--- PackagesSource/dbforge-sql-search/tools/chocolateyuninstall.ps1
+++ PackagesSource/dbforge-sql-search/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'dbForge Search for SQL Server, v2026.1.112'
+  softwareName  = 'dbForge Search for SQL Server, v2026.2.54'
   fileType      = 'exe'
 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Uninstall.log`""
