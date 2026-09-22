7490e29377d5138e53cefe9765293a2a40f64876
site <vitaliybog@devart.com>
2026-09-22 12:12:06 +0300

    Update package dbforge-sql-ep to version 2026.2.54

--- PackagesSource/dbforge-sql-ep/tools/chocolateyinstall.ps1
+++ PackagesSource/dbforge-sql-ep/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://chocolatey.devart.com/files/875F8E32991FCA8C2FB0D89884F1169719FF9EA8E3F28815E1EB5C0458FAD3A8/eventprofilersql.exe' 
+$url        = 'https://chocolatey.devart.com/files/BC3F91D19C923A456E7812228762C9B321436BB59190777CFEE46F1090FDA0F2/eventprofilersql.exe' 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url 
-  softwareName  = 'dbForge Event Profiler for SQL Server, v2026.1.112' 
-  checksum      = '875F8E32991FCA8C2FB0D89884F1169719FF9EA8E3F28815E1EB5C0458FAD3A8'
+  softwareName  = 'dbForge Event Profiler for SQL Server, v2026.2.54' 
+  checksum      = 'BC3F91D19C923A456E7812228762C9B321436BB59190777CFEE46F1090FDA0F2'
   checksumType  = 'sha256' 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Install.log`""
   validExitCodes= @(0, 3010, 1641)
--- PackagesSource/dbforge-sql-ep/tools/chocolateyuninstall.ps1
+++ PackagesSource/dbforge-sql-ep/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'dbForge Event Profiler for SQL Server, v2026.1.112'
+  softwareName  = 'dbForge Event Profiler for SQL Server, v2026.2.54'
   fileType      = 'exe'
 
   silentArgs = "/VERYSILENT /FORCECLOSEAPPLICATIONS /ngen=0 /log=`"$env:TEMP\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).Uninstall.log`""
